3eacb1c9da675656b3436991a7a8ffbb89f38bda
nhmcuong1106 <cuong.nhm2500@sinhvien.hoasen.edu.vn>
2019-06-09 12:14:52 +0700

    my first react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Home from './components/home/home'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello ReactJs</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './home.css'
+
+class Home extends Component {
+  constructor() {
+
+  }
+  render() {
+    return (
+      <div class="home">
+        <strong>Welcome 'Cuong'</strong>
+      </div>
+    )
+  }
+}
+
+export default Home;
